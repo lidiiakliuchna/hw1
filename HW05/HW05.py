90697dc2b94e027a1167c954e307bb403f2b68b8
Lidiia Kliuchna <lida.kluchnaya@gmail.com>
2020-10-11 10:25:33 +0300

    HW number 5

---
+++ HW05/HW05.py
@@ -0,0 +1,25 @@
+with open(r"D:\hw1\HW05\poem.txt", encoding="utf-8") as f:
+    lines = f.readlines()
+for index, value in enumerate(lines):
+        number_of_words = len(value.split())        
+        print(number_of_words/2) 
+list_of_lists = []
+for line in lines:
+    line = line.split()
+    list_of_lists.append(line)
+ 
+sorted_lists = (sorted(list_of_lists, key=len))
+diff = len(sorted_lists[-1])/len(sorted_lists[0])
+print(diff)    
+counter = 0   
+percentage = 0 
+with open(r"D:\hw1\HW05\poem.txt", encoding="utf-8") as f:
+    line_count = 0
+    for line in f:
+        line_count += 1
+    for index, value in enumerate(lines):
+        number_words = len(value.split()) 
+        if number_words>5: 
+            counter += 1
+    percentage = (counter * 100)/line_count
+print(percentage)
